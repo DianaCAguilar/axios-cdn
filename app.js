a15c7327a20b6a7dd6f83eafb5f53f76a5437a2c
Diana Aguilar <dianaar123@gmail.com>
2020-02-04 21:39:49 -0500

    second phase

--- app.js
+++ app.js
@@ -7,17 +7,35 @@ const p = Promise.all([p1, p2])
         return responses.map(response => response.data);
     });
 
-let companies = [];
-let products;
 
 p.then(data => {
-    companies = data[0];
-    products = data[1];
+    let companies = data[0];
+    let products = data[1];
+    render(companies);
 })
 // console.log(companies, products)
 
 const render = (event) => {
-    const html = `
+    //event.map(company => console.log(company))
+    const html = event.map(company => {
+        return `<tr>
+            <td>${company.id}</td>
+            <td>${company.name}</td>
+            <td>${company.phone}</td>
+            <td>${company.state}</td>
+        </tr>`
+    }).join('')
+    
+    let tbody = document.querySelector('TBODY')
+    tbody.innerHTML = html
+}
 
-    `
+const renderNav =(companies, products)  => {
+    const html = `<li>
+        <a href = '#companies'>Companies (${companies.length} )</a> 
+    </li>
+    <li>
+        <a href = '#products'>Products (${products.length} )</a> 
+    </li>`
+
 }
